a7bc837b7de92150522a576e029b3c9185e2631e
nasif raihan <nasif.raihan78@gmail.com>
2024-03-07 01:14:44 +0600

    registered models in the admin

--- shoppinglyx/app/admin.py
+++ shoppinglyx/app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, Customer, Product, OrderPlaced
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "name", "city", "locality", "zipcode", "division")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "category",
+        "brand",
+        "selling_price",
+        "discount_price",
+    )
+
+
+@admin.register(OrderPlaced)
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display = ("id", "quantity", "ordered_date", "status")
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "quantity")
