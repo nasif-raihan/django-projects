580e984a30e5b6c6bf668dbfa9eef890da2a2577
nasif raihan <nasif.raihan78@gmail.com>
2024-03-09 22:48:48 +0600

    implemented login functionality

--- shoppinglyx/app/forms.py
+++ shoppinglyx/app/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    UsernameField,
+)
 from django.contrib.auth.models import User
+
+from django.utils.translation import gettext_lazy
 
 
 class RegistrationForm(UserCreationForm):
@@ -20,4 +26,18 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        labels = {"password2": "Confirm Password (again)"}
         widgets = {"username": forms.TextInput(attrs={"class": "form-control"})}
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(
+        widget=forms.TextInput(attrs={"class": "form-control", "autofocus": True})
+    )
+    password = forms.CharField(
+        strip=False,
+        label=gettext_lazy("Password"),
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "auto-complete": "current-password"},
+        ),
+    )
--- shoppinglyx/app/urls.py
+++ shoppinglyx/app/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from app import forms
 from app import views
 
 urlpatterns = [
@@ -19,7 +21,13 @@ urlpatterns = [
     path("changepassword/", views.change_password, name="changepassword"),
     path("mobile/", views.mobile, name="mobile"),
     path("mobile/<slug:slug_field>", views.mobile, name="mobile-slug"),
-    path("login/", views.login, name="login"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(
+            template_name="app/login.html", authentication_form=forms.LoginForm
+        ),
+        name="login",
+    ),
     path("registration/", views.RegistrationView.as_view(), name="registration"),
     path("checkout/", views.checkout, name="checkout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
