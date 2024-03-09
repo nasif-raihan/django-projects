5be0355cf94c8d3150c0def0d9924fb8898c7f39
nasif raihan <nasif.raihan78@gmail.com>
2024-03-09 23:11:57 +0600

    implemented logout functionality

--- shoppinglyx/app/urls.py
+++ shoppinglyx/app/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
         ),
         name="login",
     ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("registration/", views.RegistrationView.as_view(), name="registration"),
     path("checkout/", views.checkout, name="checkout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
