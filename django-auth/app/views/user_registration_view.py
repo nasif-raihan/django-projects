9f4d8453e8fef9d76f4bc7c2c776ae12ee9d31b9
nasif raihan <nasif.raihan78@gmail.com>
2024-06-13 17:04:15 +0600

    implemented jwt token for registration

--- django-auth/app/views/user_registration_view.py
+++ django-auth/app/views/user_registration_view.py
@@ -1,14 +1,24 @@
+from typing import Any
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from ..serializers import UserRegistrationSerializer
+
+from ..serializers import CustomTokenObtainPairSerializer, UserRegistrationSerializer
 
 
 class UserRegistrationView(APIView):
+    def __init__(self, **kwargs: Any) -> None:
+        self.token_serializer = CustomTokenObtainPairSerializer()
+        super().__init__(**kwargs)
+
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
+            token = self.token_serializer.get_token(user)
+            access_token = str(token.access_token)
+            refresh_token = str(token)
             return Response(
                 data={
                     "user": {
@@ -20,6 +30,8 @@ class UserRegistrationView(APIView):
                         "created_at": user.created_at,
                         "updated_at": user.updated_at,
                     },
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
                     "message": "Successfully registered a new user!",
                 },
                 status=status.HTTP_201_CREATED,
