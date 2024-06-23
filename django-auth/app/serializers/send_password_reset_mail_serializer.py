a85c0d6484dd831ceaad063cd17b74b794aaeb27
Nasif Raihan <nasif.raihan78@gmail.com>
2024-06-23 14:23:43 +0600

    fixed email sending settings

--- django-auth/app/serializers/send_password_reset_mail_serializer.py
+++ django-auth/app/serializers/send_password_reset_mail_serializer.py
@@ -28,12 +28,12 @@ class SendPasswordResetMailSerializer(serializers.Serializer):
         user_id = urlsafe_base64_encode(s=force_bytes(user.id))
         token = PasswordResetTokenGenerator().make_token(user)
         link = f"http://localhost:3000/api/user/reset-password/{user_id}/{token}"
-        print(f"Reset password {link=}")
         data = {
             "subject": "Reset your password",
             "body": f"Please click the following link to reset your password: {link}",
             "to_email": user.email,
         }
+        print(f"{data=}")
         self.send_mail(data)
         return attrs
 
